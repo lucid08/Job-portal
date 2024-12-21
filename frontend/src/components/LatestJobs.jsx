ddb580039226c5bebb51b8f8d030a1c3572e88ac
Sumit Choudhary <sumit.rc619@gmail.com>
2024-12-21 23:41:49 +0530

    N

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -14,7 +14,7 @@ const LatestJobs = () => {
     <div className='max-w-7xl mx-auto my-20'>
         <h1 className='text-3xl font-bold'><span className='text-blue-600'>Latest</span> Jobs Openings</h1>
         {/* Add job cards here */}
-        <div className='grid grid-cols-3 gap-4 my-5'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-5'>
             {
 
             allJobs.length > 0 ?  allJobs.slice(0, 6).map((job) => <LatestJobCards key={job._id} job={job}/>) : <span>No Jobs Available</span>
